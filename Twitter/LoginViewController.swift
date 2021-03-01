e8bf3d19aef6b4cf42123d5670fc421c07d6e745
Ian Polidora <polidora2k@gmail.com>
2021-03-01 15:37:53 -0500

    Implemented logout, stay logged in, and basic Tweet Cell design

--- Twitter/LoginViewController.swift
+++ Twitter/LoginViewController.swift
@@ -9,16 +9,6 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
-    @IBAction func onLoginButton(_ sender: Any) {
-        let myAPIUrl = "https://api.twitter.com/oauth/request_token"
-        
-        TwitterAPICaller.client?.login(url: url, success: {
-            self.performSegue(withIdentifier: "loginToHome", sender: self)
-        }, failure: { (Error) in
-            print("Could not log in")
-        })
-    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -26,7 +16,22 @@ class LoginViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
     
-
+    override func viewDidAppear(_ animated: Bool) {
+        if(UserDefaults.standard.bool(forKey: "userLoggedIn") == true){
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }
+    }
+    
+    @IBAction func onLoginButton(_ sender: Any) {
+        let myAPIUrl = "https://api.twitter.com/oauth/request_token"
+        
+        TwitterAPICaller.client?.login(url: myAPIUrl, success: {
+            UserDefaults.standard.set(true, forKey: "userLoggedIn")
+            self.performSegue(withIdentifier: "loginToHome", sender: self)
+        }, failure: { (Error) in
+            print("Could not log in")
+        })
+    }
     /*
     // MARK: - Navigation
 
---
+++ Twitter/TweetTableViewCell.swift
@@ -0,0 +1,28 @@
+//
+//  TweetTableViewCell.swift
+//  Twitter
+//
+//  Created by Ian Polidora on 3/1/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import UIKit
+
+class TweetTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var profileNameLabel: UILabel!
+    @IBOutlet weak var tweetContentLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
